4982419cbf5912ea25fd667fba83e8e2d4c834b8
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-01-20 09:48:11 +0100

    feat(routes): add blog route group with CRUD endpoints - 013/
    - Added a route group with 'blog' prefix and 'blog.' name prefix.
    - Implemented routes for blog create, edit, and show pages.
    - Named the home route as 'home' for consistency.

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/about', function () {
     return "This a Home Page.";
@@ -13,3 +13,18 @@ Route::get('/about', function () {
 Route::get('/user/{id}/{slug}', function ($id, $slug) {
     return "This is User Page with ID: " . $id . " " . $slug;
 })->name('user');
+
+// INFO:── ROUTES GROUPS ──────────────────────────────────────────────
+Route::group(['prefix' => 'blog', 'as' => 'blog.'], function () {
+    Route::get('/create', function () {
+        return "This is Blog Create Page.";
+    })->name('create');
+
+    Route::get('/edit', function () {
+        return "This is Blog Edit Page.";
+    })->name('edit');
+
+    Route::get('/show', function () {
+        return "This is Blog Show Page.";
+    })->name('show');
+});
