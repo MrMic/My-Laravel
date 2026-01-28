a0e5ca0ee6b4b9d9f586cd030a7e00b317a4b221
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-01-28 10:11:05 +0100

    refactor(routes,controller): move logic to HomeController - 026/
    Moved the home and about page logic from route closures to dedicated
    methods in HomeController. Updated routes to use controller methods for
    better organization and maintainability.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    // ______________________________________________________________________
+    public function index(): View
+    {
+        return view('welcome');
+    }
+    // ______________________________________________________________________
+    public function about(): View
+    {
+        $title = 'About page!';
+        $description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Nunc vulputate libero et velit interdum, ac aliquet odio mattis.";
+
+        return view('about', [
+            'title' => $title,
+            'description' => $description
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 // ______________________________________________________________________
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
-
-Route::get('/about', function () {
-    $title = 'About page!';
-    $description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Nunc vulputate libero et velit interdum, ac aliquet odio mattis.";
-
-    return view('about', [
-        'title' => $title,
-        'description' => $description
-    ]);
-})->name('about');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 // ______________________________________________________________________
-Route::get('/contact', function () {
-    $title = 'Contact page!';
-    $description = "Urna, ac, adipiscing laoreet eget rutrum dapibus faucibus efficitur
-    pharetra. Ultricies, amet, dui velit blandit arcu tincidunt nunc vivamus donec.";
-    $books = [ "Le petit prince", "Le Seigneur des anneaux", "Harry Potter" ];
-
-    return view('contact.index', [
-        'title' => $title,
-        'description' => $description,
-        'books' => $books
-    ]);
-})->name('contact');
+Route::get('/about', [HomeController::class, 'about'])->name('about');
