8a1a7214ba4f05a663ee5652b468ed40d1f4b5cc
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-01-30 09:53:08 +0100

    feat(migration): add blogs table migration - 030/
    Add a new migration to create the 'blogs' table with columns for id, title,
    description, and timestamps (created_at and updated_at). This sets up the
    database structure needed for storing blog entries.

---
+++ database/migrations/2026_01_30_083143_create_blogs_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->text('description');
+            $table->timestamps(); // INFO: created_at & updated_at (2 cols created automatically)
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
