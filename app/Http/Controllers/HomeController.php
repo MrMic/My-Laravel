c390909e4f68b57c3d6c7a7b9d8c327941da4187
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-02-09 10:33:02 +0100

    refactor(home): update HomeController index to fetch and return users - 40/
       Modified the index method in HomeController to fetch users from the
       database using the DB facade. The previous test user insertion is
       commented out, and the method now returns a collection of users with
       IDs greater than or equal to 5, updating the return type hint to
       accommodate Collection and stdClass.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,18 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
+use stdClass;
 
 class HomeController extends Controller
 {
     // ______________________________________________________________________
-    public function index(): View
+    public function index(): Collection|View|stdClass
     {
-        DB::table('users')->insert([
-            'name' => 'John Doe',
-            'email' => 'john@gmail.fr',
-            'password' => bcrypt('password123'),
-        ]);
+        // DB::table('users')->insert([
+        //     'name' => 'John Doe',
+        //     'email' => 'john@gmail.fr',
+        //     'password' => bcrypt('password123'),
+        // ]);
+
+        // $users = DB::table('users')->get();
+        // $users = DB::table('users')->where('id', 5)->first();
+        $users = DB::table('users')->where('id', '>=', 5)->get();
+        return $users;
 
         return view('welcome');
     }
