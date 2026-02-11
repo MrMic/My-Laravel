7b9cb524f8d01bae66ef585446c530abdf5d7791
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-02-11 10:17:02 +0100

    feat(home): fetch and debug blog titles in HomeController index - 43/
       Removed obsolete commented-out database operation examples for users and
       implemented blog title fetching using the DB facade's pluck method. Added a
       dd() call to inspect the retrieved blog titles array.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,29 +12,9 @@ class HomeController extends Controller
     // ______________________________________________________________________
     public function index(): Collection|View|stdClass
     {
-        // INFO: Insert row
-        // DB::table('users')->insert([
-        //     'name' => 'John Doe',
-        //     'email' => 'john@gmail.fr',
-        //     'password' => bcrypt('password123'),
-        // ]);
-
-        // INFO: Get data from DB
-        // $users = DB::table('users')->get();
-        // $users = DB::table('users')->where('id', 5)->first();
-        // $users = DB::table('users')->where('id', '>=', 5)->get();
-        // return $users;
-
-        // INFO: Update row from DB
-        // DB::table('users')->where('id', 5)->update([
-        //     'name' => 'Toto',
-        //     'email' => 'toto@gmail.fr',
-        // ]);
-
-        // INFO: Delete row from DB
-        // DB::table('users')->where('id', 5)->delete();
-        // INFO: Delete multiple rows from DB
-        // DB::table('users')->where('id', '>=', 5)->delete();
+        // $blogs =  DB::table('blogs')->select('title')->limit(10)->get();
+        $blogs =  DB::table('blogs')->pluck('title', 'id')->toArray();
+        dd($blogs);
 
         return view('welcome');
     }
