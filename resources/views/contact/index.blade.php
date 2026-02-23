cdda512dcfa34f506a0a3262878a855bb3af3a22
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-02-23 11:18:58 +0100

    feat(contact): add contact page and controller - 56/ Form Markup
    - Created ContactController with index method to display the contact page.
    - Updated routes/web.php to add a route for the contact page.
    - Refactored resources/views/contact/index.blade.php to use Blade
      templating and added a contact form.

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,14 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Page</title>
-</head>
-
-<body>
-    <h2>Contact Page</h2>
-</body>
-
-</html>
+@section('contents')
+    <section>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="mt-5 mb-5 card">
+                    <div class="card-body">
+                        <form>
+                            <div class="mb-3">
+                                <label for="">Name</label>
+                                <input type="text" class="form-control" id="">
+                            </div>
+                            <div class="mb-3">
+                                <label for="">Email</label>
+                                <input type="email" class="form-control" id="">
+                            </div>
+                            <div class="mb-3">
+                                <label for="">Subject</label>
+                                <input type="text" class="form-control" id="">
+                            </div>
+                            <div class="mb-3">
+                                <label for="">Message</label>
+                                <textarea name="" class="form-control"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,10 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\SingleActionController;
-// use App\Models\MyBlog as AppMyBlog;
-use App\Models\Blog;
 use Illuminate\Support\Facades\Route;
 
 // ______________________________________________________________________
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-// ______________________________________________________________________
-Route::get('/about', [HomeController::class, 'about'])->name('about');
-
-// ______________________________________________________________________
-Route::get('/single-action', SingleActionController::class)->name('single.action');
-
-// ______________________________________________________________________
-// Route::resource('/blog', BlogController::class);
-
-// ______________________________________________________________________
-Route::get('/blog', function () {
-    // $blogs = AppMyBlog::all();
-    $blogs = Blog::all();
-    dd($blogs);
-});
+Route::get('/contact', [ContactController::class, 'index'])->name('contact.name');
